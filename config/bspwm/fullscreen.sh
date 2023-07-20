c36e38d8ab678dd49c3970ac710fc0c8794e3f54
sudo-harun <harunfelicianne@gmail.com>
2023-07-20 20:00:05 +0100

    edit a few files in bspwm branch (eww)

--- config/bspwm/fullscreen.sh
+++ config/bspwm/fullscreen.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-EWW=$HOME/.eww/target/release/eww
-
 bspc subscribe node_state | while read -r _ _ _ _ state flag; do
   if [ "$state" != "fullscreen" ]; then
     continue
   fi
 
   if [ "$flag" == on ]; then
-    $EWW -c ~/.config/eww/bar close-all
-    killall $EWW
+    eww close-all
     killall picom
   else
     xrandr --output eDP1 --auto
-    $EWW daemon
-    $EWW -c ~/.config/eww/bar open bar
     picom &
+    eww open bar
   fi
 done &
